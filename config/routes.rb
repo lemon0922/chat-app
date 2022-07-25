616550dc54f6e3cee5f349d7f933a11e276357c2
lemon0922 <ima.com0922@icloud.com>
2022-07-25 20:05:53 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"#ルートパスへのアクセスがあったら、rooms_controllerのindexアクションが呼び出される
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
