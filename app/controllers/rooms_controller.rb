621205f9aaa6d37514d304dbd2e2957212eac35f
lemon0922 <ima.com0922@icloud.com>
2022-07-25 19:20:08 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+
+  def index
+  end
+
   def new #チャットルームの新規作成なので「new」アクションを定義
     @room = Room.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"#ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
+  root to: "rooms#index"#ルートパスへのアクセスがあったら、rooms_controllerのindexアクションが呼び出される
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
