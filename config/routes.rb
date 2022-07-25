f8da9d413f376a61b0f1a874ff0655a3f2ca607d
lemon0922 <ima.com0922@icloud.com>
2022-07-25 20:56:27 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"#ルートパスへのアクセスがあったら、rooms_controllerのindexアクションが呼び出される
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
